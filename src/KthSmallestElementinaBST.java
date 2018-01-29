50fec12cc8b78d73538e9821bcdd77b2acd0770b
lewis <2328811532@qq.com>
2018-01-29 21:35:49 +0800

    088

---
+++ src/KthSmallestElementinaBST.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author lpf
+ * @Date 1/29/18 8:46 PM
+ */
+public class KthSmallestElementinaBST {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+    }
+    //todo
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> list = new ArrayList<>();
+        return 0;
+    }
+}
